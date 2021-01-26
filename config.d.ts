db38d08fc9b3e5e18f599cef074e61d86580e02c
Rico Kahler <ricokahler@me.com>
2021-01-26 11:40:29 -0500

    fix(build): add re-exports and types

---
+++ config.d.ts
@@ -0,0 +1,2 @@
+export type * from './dist/create-next-plugin-preval';
+export { default } from './dist/create-next-plugin-preval';
---
+++ config.js
@@ -0,0 +1,1 @@
+module.exports = require('./dist/create-next-plugin-preval').default;
