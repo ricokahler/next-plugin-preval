5385966967cccbe1a73fe03a1347ceb425337fd8
Rico Kahler <rakahler@umich.edu>
2021-05-27 23:12:25 -0400

    test: include example app in tests (#28)
    * test: include next.js example app build
    
    * test: ensure preval worked by running the server

---
+++ examples/example-app/next.config.js
@@ -0,0 +1,4 @@
+const createNextPluginPreval = require('next-plugin-preval/config');
+const withNextPluginPreval = createNextPluginPreval();
+
+module.exports = withNextPluginPreval(/* optionally add a next.js config */);
