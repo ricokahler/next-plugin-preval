70dc2576dbdba28ada4efb42dc7bda52931df991
Rico Kahler <ricokahler@me.com>
2021-07-08 11:48:28 -0400

    test: increase test timeout globally

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['node_modules', '<rootDir>/dist'],
+  setupFilesAfterEnv: ['./setup-jest.js'],
 };
